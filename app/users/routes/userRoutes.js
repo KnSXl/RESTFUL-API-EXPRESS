a2aaa7186cbcb286681dc552af6fe88161382d7a
Kauã Nascimento da Silva <kauasilva2861@gmail.com>
2026-01-28 14:17:10 -0300

    Feat: Add Multer

--- app/users/routes/userRoutes.js
+++ app/users/routes/userRoutes.js
@@ -1,15 +1,16 @@
 module.exports = (app) => {
     const controller = app.users.controllers.userController;
+    const upload = require('../../middlewares/multer.js');
 
     // Rota para listagem e criação de usuários
     app.route('/api/v1/users')
         .get(controller.listUsers)
-        .post(controller.addUser);
+        .post(upload.none(), controller.addUser);
     
     // Rota para operações por ID
     app.route('/api/v1/users/:id')
         .get(controller.getUser)
-        .put(controller.editUser)
-        .patch(controller.editUser)
+        .put(upload.none(), controller.editUser)
+        .patch(upload.none(), controller.editUser)
         .delete(controller.deleteUser);
 };
